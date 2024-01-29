c4e29a155c4d566788c97cfa652328d53203a45a
Leo <paivaleo7@gmail.com>
2024-01-29 16:56:50 -0300

    Components Updates

--- lib/view/login_page.dart
+++ lib/view/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:fashion/util/components.dart';
 import 'package:fashion/util/style.dart';
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
@@ -19,7 +20,7 @@ class _LoginPageState extends State<LoginPage> {
       child: Scaffold(
         appBar: AppBar(
           title: const Text(
-            'fashion',
+            'just exquisite',
             style: kAppBarTitle,
           ),
           centerTitle: true,
@@ -28,7 +29,7 @@ class _LoginPageState extends State<LoginPage> {
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
             Container(
-              padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 15),
+              padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 18),
               child: const Column(children: [
                 Text(
                   'Sign In',
@@ -38,29 +39,33 @@ class _LoginPageState extends State<LoginPage> {
                 Text('Hi! Welcome back, you\'ve been missed')
               ]),
             ),
-            FieldsLogin(
+            DefaultField(
               controller: emailController,
               name: 'E-mail',
               isPassword: false,
+              hintText: 'example@email.com',
             ),
-            FieldsLogin(
+            DefaultField(
               controller: passwordController,
               name: 'Password',
               isPassword: true,
+              hintText: '****************',
             ),
             const ForgotPassword(),
-            const DefaultButton(),
+            const DefaultButton(
+              name: 'Sign In',
+            ),
             const Padding(
               padding: EdgeInsets.symmetric(vertical: 15),
               child: Text(
-                'Get sign with',
+                'Get sign in with',
                 textAlign: TextAlign.center,
               ),
             ),
             Padding(
               padding: const EdgeInsets.symmetric(vertical: 20),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.center, // Alteração aqui
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   RoundedButton(icon: MdiIcons.apple),
                   RoundedButton(icon: MdiIcons.google),
@@ -69,14 +74,14 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 25),
+              padding: const EdgeInsets.only(top: 25),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   const Text('Don\'t have an account? '),
                   GestureDetector(
                     onTap: () {
-                      //TODO - criar conta
+                      Navigator.pushReplacementNamed(context, '/signUp');
                     },
                     child: const Text(
                       'Sign Up',
@@ -89,62 +94,6 @@ class _LoginPageState extends State<LoginPage> {
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class RoundedButton extends StatelessWidget {
-  const RoundedButton({super.key, required this.icon});
-
-  final IconData icon;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5),
-      child: GestureDetector(
-        onTap: () {},
-        child: Container(
-          padding: const EdgeInsets.all(25),
-          decoration: BoxDecoration(
-            border: Border.all(color: Colors.black),
-            borderRadius: BorderRadius.circular(50),
-          ),
-          child: Icon(
-            icon,
-            color: Colors.black,
-            size: 28,
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class DefaultButton extends StatelessWidget {
-  const DefaultButton({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 35),
-      child: ElevatedButton(
-        onPressed: () {},
-        style: ButtonStyle(
-          backgroundColor: MaterialStateProperty.all<Color>(
-              const Color.fromRGBO(0, 0, 0, 0.8)),
-        ),
-        child: const Padding(
-          padding: EdgeInsets.symmetric(vertical: 12),
-          child: Text(
-            'Sign In',
-            style: TextStyle(
-                color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
-          ),
         ),
       ),
     );
@@ -173,74 +122,6 @@ class ForgotPassword extends StatelessWidget {
           ),
           textAlign: TextAlign.end,
         ),
-      ),
-    );
-  }
-}
-
-// ignore: must_be_immutable
-class FieldsLogin extends StatefulWidget {
-  FieldsLogin(
-      {super.key,
-      required this.controller,
-      required this.name,
-      required this.isPassword});
-
-  final String name;
-  final TextEditingController controller;
-  bool isPassword = false;
-
-  @override
-  State<FieldsLogin> createState() => _FieldsLoginState();
-}
-
-class _FieldsLoginState extends State<FieldsLogin> {
-  bool obscureText = true;
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18, vertical: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            widget.name,
-            style: const TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          const SizedBox(height: 5),
-          !widget.isPassword
-              ? TextField(
-                  controller: widget.controller,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(30)),
-                    hintText: 'example@email.com',
-                  ),
-                )
-              : TextField(
-                  controller: widget.controller,
-                  obscureText: obscureText,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(30),
-                    ),
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        obscureText ? Icons.visibility : Icons.visibility_off,
-                      ),
-                      onPressed: () {
-                        setState(() {
-                          obscureText = !obscureText;
-                        });
-                      },
-                    ),
-                    hintText: '****************',
-                  ),
-                ),
-        ],
       ),
     );
   }
